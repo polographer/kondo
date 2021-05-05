e888ed7876db574369ace721d3b524333628810e
Polo Ornelas <me@poloornelas.mx>
2021-05-04 22:17:11 -0400

    first version of thumbnail

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,4 +1,6 @@
 class Picture < ApplicationRecord
+  require "image_processing/vips"
+
   belongs_to :path
   #has_one_attached :web
   has_one_attached :web do |attachable|
@@ -7,7 +9,16 @@ class Picture < ApplicationRecord
     attachable.variant :thumb_800, resize: "800x800"
   end
 
-  # def thumbnalize
+  def thumbnalize
+    if not self.full_path.blank?
+      pipeline = ImageProcessing::Vips
+        .source(self.full_path)
+        .resize_to_limit(1080, 1350)
+        .convert("jpg")
+        .saver(quality: 85)
+
+      self.web.attach( io: File.open(pipeline.call),filename: "picture_#{self.id}_web.jpg" )
+    end
     
-  # end
+  end
 end
