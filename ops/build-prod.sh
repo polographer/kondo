b8aaba27d661e5bd9cb6da3c4e801a7a0d86c53f
Ale Ornelas <me@ale.world>
2021-08-15 13:15:00 -0500

    Production docker images

--- ops/build-prod.sh
+++ ops/build-prod.sh
@@ -3,11 +3,16 @@ set -euo pipefail
 
 VERSION=0.1.0
 
-echo "Cleaning Up"
-git clean -xdf
 echo "Building Backend"
-docker build kondo:$VERSION -f Dockerfile.production .
+docker build -t kondo:$VERSION -f Dockerfile.production .
 echo "Building Assets"
-docker run --name "kondo-assets-$VERSION" "kondo:$VERSION" bundle exec rake assets:precompile
+docker run --entrypoint sh -d --name "kondo-assets-$VERSION" "kondo:$VERSION" echo "Assets Container UP"
 echo "Exporting Assets"
 docker cp "kondo-assets-$VERSION:/usr/src/app/public" ./ops/nginx/public
+echo "Cleaning container"
+docker rm "kondo-assets-$VERSION"
+echo "Building Assets Container"
+pushd ops/nginx
+docker build -t "kondo-assets:$VERSION" .
+echo "Succesfully built kondo:$VERSION"
+echo "Succesfully built kondo-assets:$VERSION"
