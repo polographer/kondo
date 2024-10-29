9abf2422c5f4e64bb56b1a0eb21916f28ad5d4e4
Marco Ornelas <marco.ornelas@ideacrew.com>
2024-10-29 11:34:13 -0400

    update gems include codecoverage

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,16 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative "../config/environment"
 require "rails/test_help"
 
+require 'simplecov'
+require 'simplecov-lcov'
+SimpleCov::Formatter::LcovFormatter.config.report_with_single_file = true
+SimpleCov.formatter = SimpleCov::Formatter::LcovFormatter
+SimpleCov.start do
+  add_filter(/^\/spec\//) # For RSpec, use `test` for MiniTest
+  enable_coverage(:branch)
+end
+
+
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
